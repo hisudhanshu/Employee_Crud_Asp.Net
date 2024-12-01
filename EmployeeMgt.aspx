8e70e9ca1b4b6120400c97a45ace9d2642a82273
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2024-12-01 12:35:00 +0530

    The GridViewList Employee Data Show Create method and event to show data.

--- EmployeeMgt.aspx
+++ EmployeeMgt.aspx
@@ -1,5 +1,30 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EmployeeMgt.aspx.cs" Inherits="OnlineTuto.EmployeeMgt" %>
-<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-</asp:Content>
 
-<%-- SET A LOGIN PAGE AS A DEFAULT OPEN GOTO LOGIN PAGE AND SET AS A FIRST PAGE --%>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <%-- SET A LOGIN PAGE AS A DEFAULT OPEN GOTO LOGIN PAGE AND SET AS A FIRST PAGE --%>
+
+    <div class="container">
+        <p>&nbsp;</p>
+        <h1 style="text-align: center;">Employee Managment Panel</h1>
+        <p>&nbsp;</p>
+        <div class="form-group" style="text-align: center;">
+            <asp:Label ID="MessageLbl" runat="server" Text=""></asp:Label>
+        </div>
+        <asp:GridView ID="GridviewList" runat="server" CssClass="table" AutoGenerateColumns="false" OnRowCommand="GridViewList_RowCommand">
+            <Columns>
+                <asp:BoundField DataField="emp_id" HeaderText="Employee ID" />
+                <asp:BoundField DataField="fullname" HeaderText="Fullname" />
+                <asp:BoundField DataField="email" HeaderText="Email" />
+                <asp:BoundField DataField="phone" HeaderText="Phone" />
+                <asp:BoundField DataField="date_of_birth" HeaderText="Date of Birth" />
+                <asp:BoundField DataField="level_of_education" HeaderText="Level of Education" />
+                <asp:BoundField DataField="salary" HeaderText="Salary" />
+                <asp:TemplateField HeaderText="Action">
+                    <ItemTemplate>
+                        <asp:Button ID="btnview" runat="server" Text="view" CommandName="ViewDetails" CommandArgument='<% Bind("emp_id")%>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
--- EmployeeMgt.aspx.cs
+++ EmployeeMgt.aspx.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,6 +13,7 @@ namespace OnlineTuto
 {
     public partial class EmployeeMgt : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection("Data Source=SUDHANSHU;Initial Catalog=OnlineTuto;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -30,7 +35,50 @@ namespace OnlineTuto
                 }
 
                 // Admin-specific functionality can be added here
+
+
+            }        //Call the method here 
+
+            if (!IsPostBack)
+            {
+                displayEmployee();
+
             }
+        }
+
+
+        //Display employee Method
+        public void displayEmployee()
+        {
+            try
+            {
+                String sqlQuery = "SELECT emp_id, fullname, email, phone, date_of_birth, level_of_education, salary FROM Employee";
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+                if (con.State == System.Data.ConnectionState.Closed)
+                    con.Open();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd); // Defined `cmd` correctly in the preceding lines
+
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                GridviewList.DataSource = dt;
+                GridviewList.DataBind();
+
+                //Close this connection here
+                con.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageLbl.Text = "There is a problem displaying employee data" + ex.Message.ToString();
+            }
+        }
+
+        //event for the GridViewList
+
+        public void GridViewList_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
         }
     }
 }
